b54fc0269a85393637003312bb51039dc2d56703
Israel Bernal <ibernal@tritansoftware.com>
2018-08-09 13:37:30 -0400

    db connection

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const express = require('express'),
       app = express();
-      mongoose = require('mongoose');
-      port = process.env.PORT || 3000,
+mongoose = require('mongoose');
+port = process.env.PORT || 3000,
       bodyParser = require('body-parser'),
       cors = require('cors');
 
 mongoose.Promises = global.Promise;
 
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds259711.mlab.com:59711/${process.env.DB_NAME}`)
-        .then( () => console.log("Connected") )
-        .catch( err => console.log( "Error: " , err ) );
+mongoose.connect(`mongodb://imbernal:israel1q2w3@ds259711.mlab.com:59711/orders`)
+      .then(() => console.log("Connected"))
+      .catch(err => console.log("Error: ", err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,4 +20,4 @@ app.use(cors());
 
 require('./src/routes')(app);
 
-app.listen(port , ()=> console.log(`Listen on ${port}`));
+app.listen(port, () => console.log(`Listen on ${port}`));
